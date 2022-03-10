c677b2a57f66442b991e8d430990723282134f24
adrgor <adrian.gorski20@gmail.com>
2022-03-10 21:25:34 +0100

    Changed strcture to use procedures

--- Pascal/BubbleSort.pas
+++ Pascal/BubbleSort.pas
@@ -1,25 +1,35 @@
 program BubbleSort;
 var numbers: array [1..50] of integer;
-i, j, temp, a, b: integer;
-begin
-        randomize();
-        for i := 0 to 49 do
-                numbers[i] := random(100);
+i, j, a, b: integer;
 
-        for i := 0 to 49 do
+procedure generateRandomNumbers(var arr: array of integer);
+begin
+        randomize();    
+        for i := 0 to length(arr) do
+                arr[i] := random(100);
+end;
+
+procedure bubbleSort(var arr: array of integer);
+begin
+        for i := 0 to length(arr)-1 do
         begin
-                for j := 0 to 49-i do
+                for j := 0 to length(arr)-1-i do
                 begin
-                        if (numbers[j] > numbers[j+1]) then
+                        if (arr[j] > arr[j+1]) then
                         begin
-                                a := numbers[j];
-                                b := numbers[j+1];
-                                numbers[j] := b;
-                                numbers[j+1] := a;
+                                a := arr[j];
+                                b := arr[j+1];
+                                arr[j] := b;
+                                arr[j+1] := a;
                         end;
                 end;
         end;
 
-        for i := 0 to 49 do
-                writeln(numbers[i])
+        for i := 0 to length(arr)-1 do
+                writeln(arr[i]);
+end;
+
+begin
+        generateRandomNumbers(numbers);
+        bubbleSort(numbers);
 end.
