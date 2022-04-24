458cc89e5b0b857475816ce8eff1824cf7899809
Adrian Górski <adrian.gorski20@gmail.com>
2022-04-24 15:39:09 +0200

    React hooks backend and frontend

---
+++ React-hooks/backend/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3002
+
+app.use(cors())
+app.use(express.json());
+
+const services = [
+    {
+        'id': 1,
+        'service_name': 'Barber',
+        'company_name': 'Barber House',
+        'price': 80
+    }, 
+    {
+        'id': 2,
+        'service_name': 'Tattoo',
+        'company_name': 'Rock\'n\'Roll tattoo',
+        'price': 200
+    },
+    {
+        'id': 3,
+        'service_name': 'Masage',
+        'company_name': 'Holy spa',
+        'price': 100
+    },
+    {
+        'id': 4,
+        'service_name': 'Manicure',
+        'company_name': 'Nobu nails',
+        'price': 50
+    }
+]
+
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(services))
+})
+
+app.post('/orders', (req,res) => {
+    console.log("Order received:")
+    console.log(req.body)
+    res.sendStatus(200)
+})
+
+app.post('/checkout', (req, res) => {
+    console.log('Payment details received ')
+    console.log(req.body)
+    res.sendStatus(200)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
